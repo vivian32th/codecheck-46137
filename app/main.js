bd06cceb9e458f88b447aef989c90ca93314a1f6
vivian32th <33028826+vivian32th@users.noreply.github.com>
2018-08-14 12:51:42 +0900

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,12 +1,10 @@
-"use strict";
-module.exports = function hello(name) {
+function hello(name) {
 /*
 * ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
   //ここにHelloworldを実装します。
-  //return "Hello World!";
-  //BEGIN_CHALLENGE
   return "Hello " + name + "!";
-  //END_CHALLENGE
-};
+}
+
+module.exports = hello;

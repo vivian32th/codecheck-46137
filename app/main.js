766c8254229c642e05cd95dbd6df3f1d7bc89b03
vivian32th <33028826+vivian32th@users.noreply.github.com>
2018-08-14 12:49:36 +0900

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,15 +1,12 @@
 "use strict";
-
-function main(argv) {
-  /**
-   * このコードは引数と標準出力を用いたサンプルコードです。
-   * このコードは好きなように編集・削除してもらって構いません。
-   *
-   * This is a sample code to use arguments and outputs.
-   * You can edit and even remove this code as you like.
-   */
-    
-  argv.forEach((v, i) => console.log(`argv[${i}]: ${v}`));
-}
-
-module.exports = main;
+module.exports = function hello(name) {
+/*
+* ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
+* nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
+*/
+  //ここにHelloworldを実装します。
+  //return "Hello World!";
+  //BEGIN_CHALLENGE
+  return "Hello " + name + "!";
+  //END_CHALLENGE
+};

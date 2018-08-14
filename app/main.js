f656b4ea32ca72d807cd6e6d619a2324f3eeb470
vivian32th <33028826+vivian32th@users.noreply.github.com>
2018-08-14 13:15:08 +0900

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,13 @@
-var hello =function main(name) {
+function hello(name) {
 /*
 * ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
   //ここにHelloworldを実装します。
-  return "Hello " + name + "!";
+  if(name == "codecheck"){
+    return "Hello codecheck!";
+  }
+  return "Hello World!";
 }
 
 module.exports = hello;
